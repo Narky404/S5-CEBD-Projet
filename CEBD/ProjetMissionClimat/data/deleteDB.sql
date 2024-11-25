abeb39e8272a9c5797f9694f52fadee60e36e706
Narky404 <esmeralda.lycee@gmail.com>
2024-11-25 16:39:37 +0100

    ajout des drop tables

--- CEBD/ProjetMissionClimat/data/deleteDB.sql
+++ CEBD/ProjetMissionClimat/data/deleteDB.sql
@@ -1,5 +1,12 @@
+--TODO Q4 Ajouter les suppressions des nouvelles tables
+DROP TABLE IF EXISTS Travaux_Departement;
+DROP TABLE IF EXISTS Communes;
+DROP TABLE IF EXISTS Travaux;
+DROP TABLE IF EXISTS Isolations;
+DROP TABLE IF EXISTS Chauffages;
+DROP TABLE IF EXISTS Photovoltaiques;
+
 DROP TABLE IF EXISTS Mesures;
 DROP TABLE IF EXISTS Departements;
 DROP TABLE IF EXISTS Regions;
 
---TODO Q4 Ajouter les suppressions des nouvelles tables
