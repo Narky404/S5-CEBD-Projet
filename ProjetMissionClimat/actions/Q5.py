29487af21b2d678ab46245d82c26ace378e09a6b
Narky404 <esmeralda.lycee@gmail.com>
2024-11-28 09:43:02 +0100

    Q5 faites

--- ProjetMissionClimat/actions/Q5.py
+++ ProjetMissionClimat/actions/Q5.py
@@ -52,49 +52,19 @@ class Window(tk.Toplevel):
         # On récupère les départements
         tab = []
         try:
-            query = """
-                SELECT code_departement, nom_departement
-                FROM Departements
-                ORDER BY code_departement
-            """
             cursor = db.data.cursor()
-            result = cursor.execute(query)
+            result = cursor.execute("""
+                SELECT DISTINCT code_departement, nom_departement, strftime('%Y', date_mesure) as annee, ROUND(avg(temperature_moy_mesure), 2) AS moyenne, min(temperature_min_mesure) AS minimum, max(temperature_max_mesure) AS maximum
+                FROM Mesures JOIN Departements USING (code_departement)
+                GROUP BY code_departement, annee
+                ORDER BY code_departement, annee
+            """)
             nbquery += 1
         except Exception as e:
             print("Erreur : " + repr(e))
-        else:
-            # Pour chaque département...
-            for dept in result:
-                # On récupère les années où il y a des mesures pour ce département
-                try:
-                    cursor2 = db.data.cursor()
-                    result2 = cursor2.execute("""
-                        SELECT DISTINCT strftime('%Y', date_mesure) as annee
-                        FROM Mesures
-                        WHERE Mesures.code_departement = ?
-                    """, [dept[0]])
-                    nbquery += 1
-                except Exception as e:
-                    print("Erreur : " + repr(e))
-                else:
-                    # Pour chaque année...
-                    for annee in result2:
-                        # On récupère les mesures pour ce département et cette année
-                        try:
-                            cursor3 = db.data.cursor()
-                            result3 = cursor3.execute("""
-                            SELECT ROUND(avg(temperature_moy_mesure), 2) AS moyenne, min(temperature_min_mesure) AS minimum, max(temperature_max_mesure) AS maximum
-                            FROM Mesures
-                            WHERE Mesures.code_departement = ? AND strftime('%Y', date_mesure) = ?
-                            """, [dept[0], annee[0]])
-                            nbquery += 1
-                        except Exception as e:
-                            print("Erreur : " + repr(e))
-                        else:
-                            # On ajoute notre mesure dans le tableau
-                            for mesures in result3:
-                                tab.append([dept[0], dept[1], annee[0], mesures[0], mesures[1], mesures[2]])
-
+        else :
+            for resultat in result:
+                tab.append( [resultat[0], resultat[1], resultat[2], resultat[3], resultat[4], resultat[5]] )
         # On affiche les données du tableau dans la treeview
         for row in tab:
             self.treeview.insert('', tk.END, values=row)
